92f2a1476345218dcf75f13a823f01132a1671db
alhabsi16 <mxmx8283@gmail.com>
2023-06-12 11:08:06 +0400

    sum2D

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace aaaa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            calculate();
+
+        }
+
+        static void calculate()
+        {
+            int sum = 0;
+            int[,] my2D = new int[3, 2];
+            my2D[0, 0] = 5;
+            my2D[0, 1] = 8;
+            my2D[1, 0] = 10;
+            my2D[1, 1] = 11;
+            my2D[2, 0] = 20;
+            my2D[2, 1] = 21;
+
+            //Console.WriteLine(my2D.Length);
+
+            for (int raw = 0; raw < my2D.GetLength(0); raw++)
+            {
+                for (int col = 0; col < my2D.GetLength(1); col++)
+                {
+                    //Console.Write(my2D[raw, col] + "  ");
+                    sum = sum + my2D[raw, col];
+
+                }
+
+            }
+            Console.WriteLine(" Total sum = " + sum);
+        }
+
+    }
+}
